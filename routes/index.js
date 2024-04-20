117e22246c1a1099cf2e913c2e44fbb064868e74
Keyber <keiberjavier26@gmail.com>
2024-04-19 23:36:41 -0400

    curriculum-Vitae

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hola mundo', fullName:'Keyber Javier Mendoza Alexandre', id:'V31294589',section:'2'});
+  res.render('index', { name: 'Keyber Mendoza', ocupation: 'Desarrollador Web Front-End', description: 'Soy estudiante de la universidad Romulo Gallegos y soy autodidacta en Desarrollo Web enfocado en programación Frontend.\nAspiro conseguir empleo en el mundo del Desarrollo Web, adquirir experiencia, nuevos conocimientos y fortalecer mi vida profesional.\n\nSoy una persona empática y entusiasta, con competencias enfocadas en el trabajo en equipo, comunicación asertiva, resolución de problemas, responsable y proactiva, me gusta aprender de los demás y superarme continuamente.' });
 });
 
 
