9369a6968a4796c28af264ebf44063ed6f5c3f9b
Keyber <keiberjavier26@gmail.com>
2024-06-22 22:28:25 -0400

    link de render

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -17,7 +17,7 @@ const ubicacion = async (req, res) => {
   }
   const config = {
   headers: {
-    'Referer': 'https://p2-v31294589-1.onrender.com/', // Establece el Referer personalizado
+    'Referer':'https://p2-v31294589-1.onrender.com/', // Establece el Referer personalizado
     'User-Agent': 'curriculumVitae' // Establece el User-Agent personalizado
   },
   timeout: 60000 // 60 segundos
